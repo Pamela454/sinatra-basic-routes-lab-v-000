cc199849bed465b958ae3d412ab927c66994658f
Pamela Frieze <pfrieze@gmail.com>
2018-06-23 21:47:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    "My name is Pam" 
+    "My name is Pam"
+  end
+
+  get '/hometwon' do
+    "My name is Pam"
   end
 
 end
